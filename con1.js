58eec85aace2ab7f87e37dc5a4dbd78ec22b69af
ReLySz531 <ReLySz531@yahoo.com>
2023-02-23 08:34:42 -0500

    commit

---
+++ con1.js
@@ -0,0 +1,39 @@
+var userChoice 
+var randomNumber
+var computerChoice
+
+var userChoice = prompt("heads or tails");
+
+var randomNumber = Math.floor(Math.random() * 1);
+
+if (randomNumber == 0) {
+    computerChoice = 'heads';
+} else {
+    computerChoice = 'tails';
+}
+
+if (randomNumber == 1) {
+    computerChoice = 'heads';
+} else {
+    computerChoice = 'tails';
+}
+
+if (userChoice == computerChoice) {
+    alert ("You guessed right! The coin flip landed on heads");
+} else {
+    alert ("Sorry, the coin clip landed on tails");
+}
+
+
+var birthYear = prompt("What year were you born?");
+
+var currentYear = 2023;
+let age = currentYear - birthYear;
+
+if (age > 21) {
+    alert ("You are old enough to drink in the US");
+} else if (age = 21) {
+    alert ("You are old enough to drink in the US...barely");
+} else if(age < 21) {
+    alert ("Sorry, you are not old enough to drink in the US");
+}
